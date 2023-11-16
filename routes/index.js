8fc3aae09c8b97e8e3b4914c372e46f0d4a78e0e
IsaiasZc <coroxerox2@gmail.com>
2023-11-16 18:49:51 -0500

    linter done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
-router.use('/patients', require('./patients'));
-router.use('/history', require('./history'));
-router.use('/appointments', require('./appointments'));
-router.use('/doctors', require('./doctors'));
-router.use('/api-docs', require('./swagger'));
+router.use('/patients', require('./patients'))
+router.use('/history', require('./history'))
+router.use('/appointments', require('./appointments'))
+router.use('/doctors', require('./doctors'))
+router.use('/api-docs', require('./swagger'))
 
-module.exports = router;
+module.exports = router
