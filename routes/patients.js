1c43ec40b79879e435d7a13e31889199b648538d
IsaiasZc <coroxerox2@gmail.com>
2023-11-18 15:19:32 -0500

    on process

--- routes/patients.js
+++ routes/patients.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
-const patientsController = require('../controllers/patients')
+const patients = require('../controllers/patients')
 
-router.post('/patient') // POST /patient
-router.put('/patient/{patientId}') // PUT /patient/{patientId}
-router.get('/patient/{patientId}/appointments') // GET /patient/{patientId}/appointments
-router.get('/patient/{patientId}/treatment') // GET /patient/{patientId}/treatment
-router.get('/patient/{patientId}') // GET /patient/{patientId}
-router.post('/patient/{patientId}') // POST /patient/{patientId}
-router.post('/patient/{patientId}/appointments') // POST /patient/{patientId}/appointments
-router.delete('/patient/{patientId}') // DELETE /patient/{patientId}
+// router.get('/patient/{patientId}/appointments') // GET /patient/{patientId}/appointments || FROM APPOINTMENTS
+// router.get('/patient/{patientId}/treatment') // GET /patient/{patientId}/treatment || FROM TREATMENT
+// router.post('/patient/{patientId}/appointments') // POST /patient/{patientId}/appointments ||  FROM APPOINTMENTS
+
+router.get('/', patients.getAllPatients)
+router.get('/:id', patients.getPatientById) // GET /patient/{patientId}
+router.post('/', patients.createPatient) // POST /patient
+router.put('/:id', patients.updatePatient) // PUT /patient/{patientId}
+router.delete('/:id', patients.deletePatient) // DELETE /patient/{patientId}
 
 module.exports = router
