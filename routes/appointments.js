42673648048db7c1df648acff4e537965d9f2452
andreyblanco <andygblanco@gmail.com>
2023-11-18 09:26:55 -0600

    appointment controllers and routes added

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const appointmentsController = require('../controllers/appointments')
 
-router.post('/appointments') // POST /appointments/
-router.get('/appointments/{patientId}') // GET /appointments/{patientId}
-router.get('/appointments/{doctorId}') // GET /appointments/{doctorId}
-router.get('/appointments/{appointmentsId}') // GET /appointments/{appointmentsId}
-router.put('/appointments/{appointmentsId}') // PUT /appointments/{appointmentsId}
-router.delete('/appointments/{appointmentsId}') // DELETE /appointments/{appointmentsId}
+router.post('/', appointmentsController.addAppointment) // POST /appointments/
+router.get('/:patientId', appointmentsController.getAppointmentsByPatientId) // GET /appointments/{patientId}
+router.get('/:doctorId', appointmentsController.getAppointmentsByDoctorId) // GET /appointments/{doctorId}
+router.get('/:id', appointmentsController.getAppointmentById) // GET /appointments/{appointmentsId}
+router.put('/:id', appointmentsController.updateAppointmentById) // PUT /appointments/{appointmentsId}
+router.delete('/:id', appointmentsController.deleteAppointment) // DELETE /appointments/{appointmentsId}
 
 module.exports = router
