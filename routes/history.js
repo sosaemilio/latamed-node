aa82aa12afe4c8114d29c81d82c978f8fc6a72ea
msramirez93 <melany.salome.ramirez@gmil.com>
2023-11-18 23:41:38 -0300

    adding routes history

--- routes/history.js
+++ routes/history.js
@@ -3,10 +3,10 @@ const historyController = require('../controllers/history');
 
 // Rutas para el historial
 router.post('/history', historyController.createNewinformation); // POST /history/
-router.get('/history/patient/:patientId', historyController.getHistory); // GET /history/patient/{patientId}
-router.get('/history/doctor/:doctorId', historyController.getSingleHistory); // GET /history/doctor/{doctorId}
-router.get('/history/:historyId', historyController.getSingleHistory); // GET /history/{historyId}
-router.put('/history/:historyId', historyController.updateNewinformation); // PUT /history/{historyId}
-router.delete('/history/:historyId', historyController.deleteNewinformation); // DELETE /history/{historyId}
+router.get('/:patientId', historyController.getHistory); // GET /history/patient/{patientId}
+router.get('/:doctorId', historyController.getSingleHistory); // GET /history/doctor/{doctorId}
+router.get('/:historyId', historyController.getSingleHistory); // GET /history/{historyId}
+router.put('/:historyId', historyController.updateNewinformation); // PUT /history/{historyId}
+router.delete('/:historyId', historyController.deleteNewinformation); // DELETE /history/{historyId}
 
 module.exports = router;
