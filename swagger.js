3d188a5afc62b2e5aeb97f39b3f8a17fa06dde61
andreyblanco <andygblanco@gmail.com>
2023-11-18 18:38:58 -0600

    schemes changed

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Emilio Sosa Movies API - BYUI'
   },
   host: 'localhost:3000',
-  schemes: ['https']
+  schemes: ['http']
 };
 
 const outputFile = './swagger-output.json';
