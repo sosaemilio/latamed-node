db85e7e7ade6119d22e9d1c90f5a64d9b7186973
msramirez93 <melany.salome.ramirez@gmil.com>
2023-11-18 22:53:31 -0300

    routesandhistory

--- routes/history.js
+++ routes/history.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const historyController = require('../controllers/history');
 
-router.post('/history'); // POST /history/
-router.get('/history/{patientId}'); // GET /history/{patientId}
-router.get('/history/{doctorId}'); // GET /history/{doctorId}
-router.get('/history/{historyId}'); // GET /history/{historyId}
-router.put('/history/{historyId}'); // PUT /history/{historyId}
-router.delete('/history/{historyId}'); // DELETE /history/{historyId}
+// Rutas para el historial
+router.post('/history', historyController.createNewinformation); // POST /history/
+router.get('/history/patient/:patientId', historyController.getHistory); // GET /history/patient/{patientId}
+router.get('/history/doctor/:doctorId', historyController.getSingleHistory); // GET /history/doctor/{doctorId}
+router.get('/history/:historyId', historyController.getSingleHistory); // GET /history/{historyId}
+router.put('/history/:historyId', historyController.updateNewinformation); // PUT /history/{historyId}
+router.delete('/history/:historyId', historyController.deleteNewinformation); // DELETE /history/{historyId}
 
 module.exports = router;
