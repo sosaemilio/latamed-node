091bec9df3943b0a50e9cd8c52ee3f482d201015
msramirez93 <melany.salome.ramirez@gmil.com>
2023-12-02 19:35:14 -0300

    Oauth in history

--- controllers/history.js
+++ controllers/history.js
@@ -1,93 +1,107 @@
 const mongodb = require('../db/connect');
+const { historyAuthenticated } = require('../middleware/historyValidator');
 const ObjectId = require('mongodb').ObjectId;
 
-const getHistory= (req, res) => {
+const getHistory = (req, res) => {
   //gethistorial
-  mongodb
-  .getDb()
-  .db('latammed')
-  .collection('medical_history')
-  .find({ username: doctorUsername00 })
-  .toArray((err,lists)=>
-  {
-    if(err){
-      res.status(400).json({message:err});
-    }
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
+  historyAuthenticated(req, res, () => {
+    // Medical History
+    mongodb
+      .getDb()
+      .db('latammed')
+      .collection('medical_history')
+      .find({ username: doctorUsername00 })
+      .toArray((err, lists) => {
+        if (err) {
+          res.status(400).json({ message: err });
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+      });
   });
 };
 
 const getSingleHistory = async (req, res) => {
   //GetSingleHistory
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-  .getDb()
-  .db('latammed')
-  .collection('medical_history')
-  .find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
+  historyAuthenticated(req, res, async () => {
+    // Obtiene el historial médico individual
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db('latammed')
+      .collection('medical_history')
+      .find({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
   });
 };
 
 const createNewinformation = async (req, res) => {
   //create newinformation
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    birthDate: req.body.birthDate,
-    email: req.body.email,
-    history: req.body.history
-    
-  };
-  const response = await mongodb.getDb().db().collection('medical_history').insertOne(contact);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
-  }
+  historyAuthenticated(req, res, async () => {
+    //create new medical information
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      birthDate: req.body.birthDate,
+      email: req.body.email,
+      history: req.body.history
+    };
+    const response = await mongodb.getDb().db().collection('medical_history').insertOne(contact);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+  });
 };
+
 
 const updateNewinformation = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  //update of information
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    birthDate: req.body.birthDate,
-    email: req.body.email,
-    history: req.body.history
-    
-  };
-  const response = await mongodb
-  .getDb()
-  .db('latammed')
-  .collection('medical_history')
-  .find({ username:  getPatientById })
-    .replaceOne({ _id:  getPatientById }, contact);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
-  }
+  historyAuthenticated(req, res, async () => {
+    // this update new medical information
+    const userId = new ObjectId(req.params.id);
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      birthDate: req.body.birthDate,
+      email: req.body.email,
+      history: req.body.history
+    };
+    const response = await mongodb
+      .getDb()
+      .db('latammed')
+      .collection('medical_history')
+      .replaceOne({ _id: userId }, contact);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    }
+  });
 };
 
+
 const deleteNewinformation = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb()
-  .db('latammed')
-  .collection('medical_history')
-  .remove({ _id:  getPatientById }, true);
-  console.log(response);
-  if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
-  }
+  historyAuthenticated(req, res, async () => {
+    // This delete all medical information 
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb()
+      .db('latammed')
+      .collection('medical_history')
+      .remove({ _id: userId }, true);
+    console.log(response);
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    }
+  });
 };
+
 
 module.exports = {
   getHistory,
