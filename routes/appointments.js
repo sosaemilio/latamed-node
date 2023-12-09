1626a6bb9183b288c1ff2e2b7d4836b24026e398
andreyblanco <andygblanco@gmail.com>
2023-12-09 01:42:38 -0600

    validations and errors

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const appointmentsController = require('../controllers/appointments');
+const appointmentsValidate = require('../middleware/appointmentsValidator');
 
-router.post('/', appointmentsController.addAppointment); // POST /appointments/
+router.post('/', appointmentsValidate.appointmentval, appointmentsController.addAppointment); // POST /appointments/
+router.get('/:id', appointmentsController.getAppointmentById); // GET /appointments/{appointmentsId}
 router.get('/:patientId', appointmentsController.getAppointmentsByPatientId); // GET /appointments/{patientId}
 router.get('/:doctorId', appointmentsController.getAppointmentsByDoctorId); // GET /appointments/{doctorId}
-router.get('/:id', appointmentsController.getAppointmentById); // GET /appointments/{appointmentsId}
 router.put('/:id', appointmentsController.updateAppointmentById); // PUT /appointments/{appointmentsId}
 router.delete('/:id', appointmentsController.deleteAppointment); // DELETE /appointments/{appointmentsId}
 
