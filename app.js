a1092afeca52548333e3fd586bc440be3fb80eeb
IsaiasZc <coroxerox2@gmail.com>
2023-12-09 17:02:01 -0500

    app modified

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const routes = require('./routes/index');
-const mongodb = require('./db/connect');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const routes = require('./routes/index')
+const mongodb = require('./db/connect')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', routes);
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/', routes)
 
 mongodb.initDb((err) => {
   if (err) {
-    console.log(err);
+    console.log(err)
   } else {
     app.listen(process.env.PORT || port, () => {
-      console.log(`Web server is listeng at port ${process.env.PORT || port}`);
-    });
+      console.log(`Web server is listeng at port ${process.env.PORT || port}`)
+    })
   }
-});
+})
+
+module.exports = app
