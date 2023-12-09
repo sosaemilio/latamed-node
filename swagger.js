9380f90219a0e276515fd223c438224be752ab51
Emilio Sosa <45885030+sosaemilio@users.noreply.github.com>
2023-12-09 18:57:10 -0400

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,18 +1,18 @@
-const swaggerAutogen = require('swagger-autogen')()
+const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
     title: 'LatamMed API',
     description: 'Medical Appoinments API'
   },
-  host: process.env.HOST,
-  schemes: [process.env.SCHEME]
-}
+  host: 'localhost:3000',
+  schemes: ['http']
+};
 
-const outputFile = './swagger-output.json'
-const routes = ['./routes/index.js']
+const outputFile = './swagger-output.json';
+const routes = ['./routes/index.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
-swaggerAutogen(outputFile, routes, doc)
+swaggerAutogen(outputFile, routes, doc);
