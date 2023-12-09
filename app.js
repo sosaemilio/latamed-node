693cd01ff99fd29996b222c91f2246b4659f9af1
Emilio Sosa <emiliossosa85@gmail.com>
2023-12-09 11:00:47 -0400

    OAuth configuration

--- app.js
+++ app.js
@@ -7,9 +7,20 @@ const mongodb = require('./db/connect');
 const app = express();
 const port = 3000;
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(auth(config));
 app.use('/', routes);
 
 mongodb.initDb((err) => {
