d687c22eee796711991e2a3387d02af78a82faa1
Emilio Sosa <emiliossosa85@gmail.com>
2023-12-09 21:12:52 -0400

    Remove empty file

--- __tests__/doctors.spec.js
+++ __tests__/doctors.spec.js
@@ -2,14 +2,10 @@ const app = require('../app')
 const supertest = require('supertest');
 const { expect } = require('@jest/globals');
 const request = supertest(app);
-const { MongoClient } = require('mongodb');
-const dotenv = require('dotenv');
-dotenv.config();
 
 describe('Test Doctor Get', () => {
     test('responds to /doctors', async () => {
         const res = await request.get('/doctors');
-        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200)
     })
 })
